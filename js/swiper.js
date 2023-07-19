a394b0c69f0e0e98d5a5328cb6035d1f84e53ac1
Su <anima951201@gmail.com>
2023-07-19 21:39:49 +0800

    20230719

--- js/swiper.js
+++ js/swiper.js
@@ -23,9 +23,16 @@ var mySwiper2 = new Swiper(".card-container", {
   slidesPerView: 4, // 一個頁面要展示的卡片數量
   // centeredSlides: true, // 把展示的卡片置中
   // spaceBetween: 30, // 卡片與卡片之間的距離
-  loop: true, // 無限循環
-  autoplay: true,
-  speed: 3000,
+  // loop: true, // 無限循環
+  // autoplay: true,
+  // speed: 3000,
   // effect: 'slide'
+  spaceBetween: 10,
+  pagination: {
+    el: '.card-pagination',
+    clickable: true,
+  },
+
+
 });
 
